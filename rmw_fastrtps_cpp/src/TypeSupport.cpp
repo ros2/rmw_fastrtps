773276f5e20053d40405992b0522375b8f5aed14
Morgan Quigley <morgan@osrfoundation.org>
2016-06-06 17:42:07 -0700

    expand the bitmask generation operation for alignment, since unary minus on windows gives a compiler warning (#38)

--- rmw_fastrtps_cpp/src/TypeSupport.cpp
+++ rmw_fastrtps_cpp/src/TypeSupport.cpp
@@ -22,7 +22,7 @@ static inline void*
 align_(size_t __align, size_t __size, void*& __ptr, size_t& __space) noexcept
 {
     const auto __intptr = reinterpret_cast<uintptr_t>(__ptr);
-    const auto __aligned = (__intptr - 1u + __align) & -__align;
+    const auto __aligned = (__intptr - 1u + __align) & ~(__align - 1);
     const auto __diff = __aligned - __intptr;
     if ((__size + __diff) > __space)
         return nullptr;
