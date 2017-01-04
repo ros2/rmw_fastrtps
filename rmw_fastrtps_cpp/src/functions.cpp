60550fe56ed90b80071b16675dd2be3662ae8369
Mikael Arguedas <mikael.arguedas@gmail.com>
2017-01-04 19:39:31 +0100

    cpplint

--- rmw_fastrtps_cpp/src/functions.cpp
+++ rmw_fastrtps_cpp/src/functions.cpp
@@ -1323,7 +1323,7 @@ rmw_create_waitset(size_t max_conditions)
   waitset->data = rmw_allocate(sizeof(CustomWaitsetInfo));
   // This should default-construct the fields of CustomWaitsetInfo
   waitset_info = static_cast<CustomWaitsetInfo *>(waitset->data);
-  RMW_TRY_PLACEMENT_NEW(waitset_info, waitset_info, goto fail, CustomWaitsetInfo,)
+  RMW_TRY_PLACEMENT_NEW(waitset_info, waitset_info, goto fail, CustomWaitsetInfo, )
   if (!waitset_info) {
     RMW_SET_ERROR_MSG("failed to construct waitset info struct");
     goto fail;
