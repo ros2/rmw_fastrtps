d8f154df2b6a1fec78c4a5e8ecf1bfb65207c2dc
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-05-18 08:51:37 -0700

    remove obsolete stuff from CMake module

--- fastrtps_cmake_module/cmake/Modules/FindFastRTPS.cmake
+++ fastrtps_cmake_module/cmake/Modules/FindFastRTPS.cmake
@@ -16,22 +16,10 @@
 #
 # CMake module for finding eProsima FastRTPS.
 #
-# Input variables:
-#
-# - FASTRTPSHOME (optional): When specified, header files and libraries
-#   will be searched for in `${FASTRTPSHOME}/include`
-#   and `${FASTRTPSHOME}/lib` respectively.
-#
 # Output variables:
 #
 # - FastRTPS_FOUND: flag indicating if the package was found
 # - FastRTPS_INCLUDE_DIR: Paths to the header files
-# - FastRTPS_HOME: Root directory for the eProsima FastRTPS install. Might be
-#   not set.
-# - FastRTPS_LIBRARIES: Name to the C++ libraries including the path
-# - FastRTPS_LIBRARY_DIRS: Paths to the libraries
-# - FastRTPS_LIBRARY_DIR: Path to libraries; guaranteed to be a single path
-# - FastRTPS_DEFINITIONS: Definitions to be passed on
 #
 # Example usage:
 #
@@ -45,24 +33,8 @@
 
 set(FastRTPS_FOUND FALSE)
 
-# -----------------------------------------
-# Detect environment variable
-# -----------------------------------------
-if($ENV{FASTRPCHOME})
-    set(FastRTPS_HOME $ENV{FASTRPCHOME})
-endif()
-
-# -----------------------------------------
-# Search for eProsima FastRTPS include DIR
-# -----------------------------------------
-set(_fastrtps_INCLUDE_SEARCH_DIRS "")
-
-if(FastRTPS_HOME)
-    list(APPEND _fastrtps_INCLUDE_SEARCH_DIRS ${FastRTPS_HOME}/include)
-endif()
-
 find_path(FastRTPS_INCLUDE_DIR
-    NAMES fastrtps/)
+  NAMES fastrtps/)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FastRTPS
