b89809a04fffa41845442e470a2404c56b41ddd3
Karsten Knese <karsten.knese@googlemail.com>
2017-05-31 17:41:27 -0700

    use rfind from pos 0

--- rmw_fastrtps_cpp/src/functions.cpp
+++ rmw_fastrtps_cpp/src/functions.cpp
@@ -554,7 +554,7 @@ _filter_ros_prefix(const std::string & topic_name)
 {
   auto prefixes = {ros_topic_prefix, ros_service_requester_prefix, ros_service_response_prefix};
   for (auto prefix : prefixes) {
-    if (topic_name.rfind(std::string(prefix) + "/") == 0) {
+    if (topic_name.rfind(std::string(prefix) + "/", 0) == 0) {
       return topic_name.substr(strlen(ros_topic_prefix));
     }
   }
