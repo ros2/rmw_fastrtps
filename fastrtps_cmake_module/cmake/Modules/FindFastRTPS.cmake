b9b7f9aacf434971f45de0e98c8830445acc97bd
Esteve Fernandez <esteve@apache.org>
2017-06-13 21:01:43 +0200

    Look for debug versions of fastcdr and fastrtps as well (#116)
    * Revert "Merge pull request #115 from ros2/revert-75-export-libraries"
    
    This reverts commit 321ca7eb006055be4d8519f0993bc1de4e49246d, reversing
    changes made to 71494911b322cad0f856df5fcd81a1ae4d64505d.
    
    * Look for debug versions of fastcdr and fastrtps as well
    
    * Split library variables into release and debug
    
    * Join FastRTPS_LIBRARIES_RELEASE and FastRTPS_LIBRARIES_DEBUG back into FastRTPS_LIBRARIES
    
    * Export debug/optimized libraries separately
    
    * Reduce if nesting
    
    * Explicitly use 'd' as a suffix for debug libraries
    
    * Fix indentation

--- fastrtps_cmake_module/cmake/Modules/FindFastRTPS.cmake
+++ fastrtps_cmake_module/cmake/Modules/FindFastRTPS.cmake
@@ -36,9 +36,65 @@ set(FastRTPS_FOUND FALSE)
 find_path(FastRTPS_INCLUDE_DIR
   NAMES fastrtps/)
 
+find_package(fastcdr REQUIRED CONFIG)
+find_package(fastrtps REQUIRED CONFIG)
+
+string(REGEX MATCH "^[0-9]+\\.[0-9]+" fastcdr_MAJOR_MINOR_VERSION "${fastcdr_VERSION}")
+string(REGEX MATCH "^[0-9]+\\.[0-9]+" fastrtps_MAJOR_MINOR_VERSION "${fastrtps_VERSION}")
+
+find_library(FastCDR_LIBRARY_RELEASE
+  NAMES fastcdr-${fastcdr_MAJOR_MINOR_VERSION} fastcdr)
+
+find_library(FastCDR_LIBRARY_DEBUG
+  NAMES fastcdrd-${fastcdr_MAJOR_MINOR_VERSION})
+
+if(FastCDR_LIBRARY_RELEASE AND FastCDR_LIBRARY_DEBUG)
+  set(FastCDR_LIBRARIES
+    optimized ${FastCDR_LIBRARY_RELEASE}
+    debug ${FastCDR_LIBRARY_DEBUG}
+  )
+elseif(FastCDR_LIBRARY_RELEASE)
+  set(FastCDR_LIBRARIES
+    ${FastCDR_LIBRARY_RELEASE}
+  )
+elseif(FastCDR_LIBRARY_DEBUG)
+  set(FastCDR_LIBRARIES
+    ${FastCDR_LIBRARY_DEBUG}
+  )
+else()
+  set(FastCDR_LIBRARIES "")
+endif()
+
+find_library(FastRTPS_LIBRARY_RELEASE
+  NAMES fastrtps-${fastrtps_MAJOR_MINOR_VERSION} fastrtps)
+
+find_library(FastRTPS_LIBRARY_DEBUG
+  NAMES fastrtpsd-${fastrtps_MAJOR_MINOR_VERSION})
+
+if(FastRTPS_LIBRARY_RELEASE AND FastRTPS_LIBRARY_DEBUG)
+  set(FastRTPS_LIBRARIES
+    optimized ${FastRTPS_LIBRARY_RELEASE}
+    debug ${FastRTPS_LIBRARY_DEBUG}
+    ${FastCDR_LIBRARIES}
+  )
+elseif(FastRTPS_LIBRARY_RELEASE)
+  set(FastRTPS_LIBRARIES
+    ${FastRTPS_LIBRARY_RELEASE}
+    ${FastCDR_LIBRARIES}
+  )
+elseif(FastRTPS_LIBRARY_DEBUG)
+  set(FastRTPS_LIBRARIES
+    ${FastRTPS_LIBRARY_DEBUG}
+    ${FastCDR_LIBRARIES}
+  )
+else()
+  set(FastRTPS_LIBRARIES "")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FastRTPS
   FOUND_VAR FastRTPS_FOUND
   REQUIRED_VARS
     FastRTPS_INCLUDE_DIR
+    FastRTPS_LIBRARIES
 )
