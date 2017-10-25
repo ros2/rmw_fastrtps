e1eba0fb84d5a1527cb9e5e5849c8a5e97ee19ec
Sriram Raghunathan <rsriram7@visteon.com>
2017-10-25 14:08:43 +0530

    The changes follow the discussions
    https://github.com/ros2/rmw/pull/124
    
    * The API defined is used to set the logging level particular to the DDS
    implementation.
    * This change is specific to DDS type Fast-RTPS.
    * The file is named as rmw_configure since we could write API's of type
      'system level config setting' types here.
    
    Signed-off-by: Sriram Raghunathan <rsriram7@visteon.com>

--- rmw_fastrtps_cpp/src/rmw_fastrtps_configure.cpp
+++ rmw_fastrtps_cpp/src/rmw_fastrtps_configure.cpp
@@ -1,3 +1,17 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "rmw/rmw.h"
 
 #include "fastrtps/log/Log.h"
@@ -6,26 +20,37 @@ extern "C"
 {
 using eprosima::fastrtps::Log;
 
-eprosima::fastrtps::Log::Kind rmw_convert_severity_fastrtps(rmw_log_level_t level)
+eprosima::fastrtps::Log::Kind rmw_convert_severity_type(rmw_log_severity_t severity)
 {
-  switch(level)
+  switch(severity)
   {
-  case RMW_LOG_LEVEL_ERROR:
-       return Log::Kind::Error;
-  case RMW_LOG_LEVEL_WARNING:
+  case RMW_LOG_SEVERITY_WARN:
        return Log::Kind::Warning;
-  case RMW_LOG_LEVEL_INFO:
+  case RMW_LOG_SEVERITY_INFO:
        return Log::Kind::Info;
+/* Fast-RTPS supports the following logging 'Kind's.
+ * Error : Max priority
+ * Warning : Medium priority
+ * Info : Low priority
+ * From rmw logging severity there are FATAL & DEBUG types as well
+ * We map them to ERROR type of Fast-RTPS which has maximum priority
+ */
+  case RMW_LOG_SEVERITY_ERROR:
+  case RMW_LOG_SEVERITY_FATAL:
+  case RMW_LOG_SEVERITY_DEBUG:
+       return Log::Kind::Error;
   default:
        //Fallback to Info if undefined types
        return Log::Kind::Info;
   }
 }
 
-void
-rmw_setup_fastrtps(rmw_log_level_t level)
+rmw_ret_t
+rmw_set_log_severity(rmw_log_severity_t severity)
 {
-  Log::Kind lvl = rmw_convert_severity_fastrtps(level);
-   eprosima::fastrtps::Log::SetVerbosity(lvl);
+  Log::Kind _severity = rmw_convert_severity_type(severity);
+  eprosima::fastrtps::Log::SetVerbosity(_severity);
+
+  return RMW_RET_OK;
 }
-}
+} // extern "C"
