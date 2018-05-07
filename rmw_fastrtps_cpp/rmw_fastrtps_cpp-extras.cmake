3700d75e28965f87876d3de3490b35291a157057
Mikael Arguedas <mikael@osrfoundation.org>
2018-05-07 08:54:31 -0700

    Revert "Export rmw_fastrtps_cpp target" (#200)
    * Revert "Support access control  (#197)"
    
    This reverts commit a9c33de826339905b737b8ab01c4719b8a240aed.
    
    * Revert "Export rmw_fastrtps_cpp target (#198)"
    
    This reverts commit a69bab1a6c493efc8b82cb5036e4e95d0e1c5c46.

--- rmw_fastrtps_cpp/rmw_fastrtps_cpp-extras.cmake
+++ rmw_fastrtps_cpp/rmw_fastrtps_cpp-extras.cmake
@@ -14,4 +14,11 @@
 
 # copied from rmw_fastrtps_cpp/rmw_fastrtps_cpp-extras.cmake
 
-list(APPEND rmw_fastrtps_cpp_LIBRARIES rmw_fastrtps_cpp::rmw_fastrtps_cpp)
+find_package(fastrtps_cmake_module REQUIRED)
+find_package(fastcdr REQUIRED CONFIG)
+find_package(fastrtps REQUIRED CONFIG)
+find_package(FastRTPS REQUIRED MODULE)
+
+list(APPEND rmw_fastrtps_cpp_INCLUDE_DIRS ${FastRTPS_INCLUDE_DIR})
+# specific order: dependents before dependencies
+list(APPEND rmw_fastrtps_cpp_LIBRARIES fastrtps fastcdr)
