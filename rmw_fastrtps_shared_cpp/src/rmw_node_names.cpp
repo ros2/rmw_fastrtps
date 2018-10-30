9150c5ade19ffad537b6e55e375852fb6cab2049
William Woodall <william@osrfoundation.org>
2018-10-29 22:32:14 -0500

    use new error handling API from rcutils
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rmw_fastrtps_shared_cpp/src/rmw_node_names.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_node_names.cpp
@@ -66,14 +66,14 @@ __rmw_get_node_names(
   rcutils_ret_t rcutils_ret =
     rcutils_string_array_init(node_names, participant_names.size() + 1, &allocator);
   if (rcutils_ret != RCUTILS_RET_OK) {
-    RMW_SET_ERROR_MSG(rcutils_get_error_string_safe())
+    RMW_SET_ERROR_MSG(rcutils_get_error_string().str)
     goto fail;
   }
 
   rcutils_ret =
     rcutils_string_array_init(node_namespaces, participant_names.size() + 1, &allocator);
   if (rcutils_ret != RCUTILS_RET_OK) {
-    RMW_SET_ERROR_MSG(rcutils_get_error_string_safe())
+    RMW_SET_ERROR_MSG(rcutils_get_error_string().str)
     goto fail;
   }
 
@@ -97,7 +97,7 @@ fail:
     if (rcutils_ret != RCUTILS_RET_OK) {
       RCUTILS_LOG_ERROR_NAMED(
         "rmw_connext_cpp",
-        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe());
+        "failed to cleanup during error handling: %s", rcutils_get_error_string().str);
       rcutils_reset_error();
     }
   }
@@ -106,7 +106,7 @@ fail:
     if (rcutils_ret != RCUTILS_RET_OK) {
       RCUTILS_LOG_ERROR_NAMED(
         "rmw_connext_cpp",
-        "failed to cleanup during error handling: %s", rcutils_get_error_string_safe());
+        "failed to cleanup during error handling: %s", rcutils_get_error_string().str);
       rcutils_reset_error();
     }
   }
--- rmw_fastrtps_shared_cpp/src/rmw_topic_names_and_types.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_topic_names_and_types.cpp
@@ -53,7 +53,7 @@ __rmw_get_topic_names_and_types(
     return RMW_RET_INVALID_ARGUMENT;
   }
   if (!node) {
-    RMW_SET_ERROR_MSG_ALLOC("null node handle", *allocator)
+    RMW_SET_ERROR_MSG("null node handle")
     return RMW_RET_INVALID_ARGUMENT;
   }
 
@@ -64,7 +64,7 @@ __rmw_get_topic_names_and_types(
 
   // Get participant pointer from node
   if (node->implementation_identifier != identifier) {
-    RMW_SET_ERROR_MSG_ALLOC("node handle not from this implementation", *allocator);
+    RMW_SET_ERROR_MSG("node handle not from this implementation")
     return RMW_RET_ERROR;
   }
 
@@ -116,7 +116,7 @@ __rmw_get_topic_names_and_types(
         if (rmw_ret != RMW_RET_OK) {
           RCUTILS_LOG_ERROR_NAMED(
             "rmw_fastrtps_shared_cpp",
-            "error during report of error: %s", rmw_get_error_string_safe());
+            "error during report of error: %s", rmw_get_error_string().str);
         }
       };
     // Setup demangling functions based on no_demangle option
@@ -135,7 +135,7 @@ __rmw_get_topic_names_and_types(
       // Duplicate and store the topic_name
       char * topic_name = rcutils_strdup(demangle_topic(topic_n_types.first).c_str(), *allocator);
       if (!topic_name) {
-        RMW_SET_ERROR_MSG_ALLOC("failed to allocate memory for topic name", *allocator);
+        RMW_SET_ERROR_MSG("failed to allocate memory for topic name");
         fail_cleanup();
         return RMW_RET_BAD_ALLOC;
       }
@@ -147,7 +147,7 @@ __rmw_get_topic_names_and_types(
           topic_n_types.second.size(),
           allocator);
         if (rcutils_ret != RCUTILS_RET_OK) {
-          RMW_SET_ERROR_MSG(rcutils_get_error_string_safe())
+          RMW_SET_ERROR_MSG(rcutils_get_error_string().str)
           fail_cleanup();
           return rmw_convert_rcutils_ret_to_rmw_ret(rcutils_ret);
         }
@@ -157,7 +157,7 @@ __rmw_get_topic_names_and_types(
       for (const auto & type : topic_n_types.second) {
         char * type_name = rcutils_strdup(demangle_type(type).c_str(), *allocator);
         if (!type_name) {
-          RMW_SET_ERROR_MSG_ALLOC("failed to allocate memory for type name", *allocator)
+          RMW_SET_ERROR_MSG("failed to allocate memory for type name")
           fail_cleanup();
           return RMW_RET_BAD_ALLOC;
         }
