75904af7b1a3956e3ed96ef351fd5eb60def0561
Jacob Perron <jacob@openrobotics.org>
2019-06-28 10:16:10 -0700

    Use rcpputils::find_and_replace instead of std::regex_replace (#291)
    The rcpputils implementation is more performant.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rmw_fastrtps_shared_cpp/src/demangle.cpp
+++ rmw_fastrtps_shared_cpp/src/demangle.cpp
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 #include <algorithm>
-#include <regex>
 #include <string>
 #include <vector>
 
+#include "rcpputils/find_and_replace.hpp"
 #include "rcutils/logging_macros.h"
 #include "rcutils/types.h"
 
@@ -46,7 +46,7 @@ _demangle_if_ros_type(const std::string & dds_type_string)
   }
 
   std::string type_namespace = dds_type_string.substr(0, substring_position);
-  type_namespace = std::regex_replace(type_namespace, std::regex("::"), "/");
+  type_namespace = rcpputils::find_and_replace(type_namespace, "::", "/");
   size_t start = substring_position + substring.size();
   std::string type_name = dds_type_string.substr(start, dds_type_string.length() - 1 - start);
   return type_namespace + type_name;
@@ -145,7 +145,7 @@ _demangle_service_type_only(const std::string & dds_type_name)
   // everything checks out, reformat it from '[type_namespace::]dds_::<type><suffix>'
   // to '[type_namespace/]<type>'
   std::string type_namespace = dds_type_name.substr(0, ns_substring_position);
-  type_namespace = std::regex_replace(type_namespace, std::regex("::"), "/");
+  type_namespace = rcpputils::find_and_replace(type_namespace, "::", "/");
   size_t start = ns_substring_position + ns_substring.length();
   std::string type_name = dds_type_name.substr(start, suffix_position - start);
   return type_namespace + type_name;
