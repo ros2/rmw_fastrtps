455d2149f86b58fa2ca5ddae393cc352c0c1a13b
jaisontj <jaisontj92@gmail.com>
2019-10-14 08:21:19 -0700

    Added new functions which can be used to get rmw_qos_profile_t from WriterQos and ReaderQos (#328)
    Signed-off-by: Jaison Titus <jaisontj92@gmail.com>

--- rmw_fastrtps_shared_cpp/src/qos.cpp
+++ rmw_fastrtps_shared_cpp/src/qos.cpp
@@ -14,6 +14,7 @@
 
 #include <limits>
 
+#include "qos_converter.hpp"
 #include "rmw_fastrtps_shared_cpp/qos.hpp"
 
 #include "fastrtps/attributes/PublisherAttributes.h"
@@ -181,53 +182,7 @@ dds_attributes_to_rmw_qos(
       break;
   }
   qos->depth = static_cast<size_t>(dds_qos.topic.historyQos.depth);
-
-  switch (dds_qos.qos.m_reliability.kind) {
-    case eprosima::fastrtps::BEST_EFFORT_RELIABILITY_QOS:
-      qos->reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
-      break;
-    case eprosima::fastrtps::RELIABLE_RELIABILITY_QOS:
-      qos->reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
-      break;
-    default:
-      qos->reliability = RMW_QOS_POLICY_RELIABILITY_UNKNOWN;
-      break;
-  }
-
-  switch (dds_qos.qos.m_durability.kind) {
-    case eprosima::fastrtps::TRANSIENT_LOCAL_DURABILITY_QOS:
-      qos->durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
-      break;
-    case eprosima::fastrtps::VOLATILE_DURABILITY_QOS:
-      qos->durability = RMW_QOS_POLICY_DURABILITY_VOLATILE;
-      break;
-    default:
-      qos->durability = RMW_QOS_POLICY_DURABILITY_UNKNOWN;
-      break;
-  }
-
-  qos->deadline.sec = dds_qos.qos.m_deadline.period.seconds;
-  qos->deadline.nsec = dds_qos.qos.m_deadline.period.nanosec;
-
-  qos->lifespan.sec = dds_qos.qos.m_lifespan.duration.seconds;
-  qos->lifespan.nsec = dds_qos.qos.m_lifespan.duration.nanosec;
-
-  switch (dds_qos.qos.m_liveliness.kind) {
-    case eprosima::fastrtps::AUTOMATIC_LIVELINESS_QOS:
-      qos->liveliness = RMW_QOS_POLICY_LIVELINESS_AUTOMATIC;
-      break;
-    case eprosima::fastrtps::MANUAL_BY_PARTICIPANT_LIVELINESS_QOS:
-      qos->liveliness = RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE;
-      break;
-    case eprosima::fastrtps::MANUAL_BY_TOPIC_LIVELINESS_QOS:
-      qos->liveliness = RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC;
-      break;
-    default:
-      qos->liveliness = RMW_QOS_POLICY_LIVELINESS_UNKNOWN;
-      break;
-  }
-  qos->liveliness_lease_duration.sec = dds_qos.qos.m_liveliness.lease_duration.seconds;
-  qos->liveliness_lease_duration.nsec = dds_qos.qos.m_liveliness.lease_duration.nanosec;
+  dds_qos_to_rmw_qos(dds_qos.qos, qos);
 }
 
 template
