0bf81122039925b1fac3690a0d0b4494d5734de4
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-03-27 14:03:55 -0300

    Use static_cast instead of reinterpret_cast
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw_fastrtps_dynamic_cpp/src/publisher.cpp
+++ rmw_fastrtps_dynamic_cpp/src/publisher.cpp
@@ -177,7 +177,7 @@ rmw_fastrtps_dynamic_cpp::create_publisher(
   rmw_publisher->can_loan_messages = false;
   rmw_publisher->implementation_identifier = eprosima_fastrtps_identifier;
   rmw_publisher->data = info;
-  rmw_publisher->topic_name = reinterpret_cast<char *>(rmw_allocate(strlen(topic_name) + 1));
+  rmw_publisher->topic_name = static_cast<char *>(rmw_allocate(strlen(topic_name) + 1));
 
   if (!rmw_publisher->topic_name) {
     RMW_SET_ERROR_MSG("failed to allocate memory for publisher topic name");
