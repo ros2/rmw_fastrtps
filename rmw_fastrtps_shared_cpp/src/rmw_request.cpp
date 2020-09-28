486d9d8e5ae51a007458c7678c223e7407b26ed7
lobotuerk <jtlorente@ekumenlabs.com>
2020-09-28 18:38:29 -0300

    reviews
    Signed-off-by: lobotuerk <jtlorente@ekumenlabs.com>

--- rmw_fastrtps_shared_cpp/src/rmw_request.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_request.cpp
@@ -41,7 +41,7 @@ __rmw_send_request(
 {
   RMW_CHECK_ARGUMENT_FOR_NULL(client, RMW_RET_INVALID_ARGUMENT);
   RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
-    client handle,
+    client,
     client->implementation_identifier, identifier,
     return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
   RMW_CHECK_ARGUMENT_FOR_NULL(ros_request, RMW_RET_INVALID_ARGUMENT);
