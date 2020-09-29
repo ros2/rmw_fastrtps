6b6c04b85f51697d5f48d8e8680e0c664fb2e1ce
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2020-09-29 09:58:56 +0200

    checked client implementation and return RMW_RET_INCORRECT_RMW_IMPLEMENTATION (#451)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rmw_fastrtps_shared_cpp/src/rmw_service_server_is_available.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_service_server_is_available.cpp
@@ -49,12 +49,17 @@ __rmw_service_server_is_available(
   RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
     node handle,
     node->implementation_identifier, identifier,
-    return RMW_RET_ERROR);
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
 
   if (!client) {
     RMW_SET_ERROR_MSG("client handle is null");
     return RMW_RET_ERROR;
   }
+
+  RMW_CHECK_TYPE_IDENTIFIERS_MATCH(
+    client handle,
+    client->implementation_identifier, identifier,
+    return RMW_RET_INCORRECT_RMW_IMPLEMENTATION);
 
   if (!is_available) {
     RMW_SET_ERROR_MSG("is_available is null");
