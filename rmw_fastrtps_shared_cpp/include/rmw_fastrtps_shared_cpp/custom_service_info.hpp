7d97d8c7d961dd9f3deaeaf0ff8238972197da8f
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2020-10-29 08:13:45 +0100

    Remove unnecessary member
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/custom_service_info.hpp
+++ rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/custom_service_info.hpp
@@ -145,7 +145,6 @@ public:
   }
 
 private:
-  CustomServiceInfo * info_;
   std::mutex mutex_;
   subscriptions_set_t subscriptions_ RCPPUTILS_TSA_GUARDED_BY(mutex_);
   clients_endpoints_map_t clients_endpoints_ RCPPUTILS_TSA_GUARDED_BY(mutex_);
