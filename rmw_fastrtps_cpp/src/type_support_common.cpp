69a7be8cba01cd383af3acc85e46844c5bbe5067
Miguel Company <miguelcompany@eprosima.com>
2021-02-04 22:49:36 +0100

    Capture cdr exceptions (#505)
    * Capturing fastcdr exceptions.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- rmw_fastrtps_cpp/src/type_support_common.cpp
+++ rmw_fastrtps_cpp/src/type_support_common.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <fastcdr/exceptions/Exception.h>
+
 #include <string>
 
 #include "rmw/error_handling.h"
