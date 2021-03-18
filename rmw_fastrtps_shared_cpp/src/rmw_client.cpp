9cefd7edde53ba045a1dc54e6ec1d163920c88bc
Miguel Company <MiguelCompany@eprosima.com>
2021-03-18 12:25:34 +0100

    Improvements on destroy_client
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- rmw_fastrtps_shared_cpp/src/rmw_client.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_client.cpp
@@ -40,8 +40,21 @@ __rmw_destroy_client(
 {
   rmw_ret_t final_ret = RMW_RET_OK;
   auto common_context = static_cast<rmw_dds_common::Context *>(node->context->impl->common);
+  auto participant_info =
+    static_cast<CustomParticipantInfo *>(node->context->impl->participant_info);
   auto info = static_cast<CustomClientInfo *>(client->data);
-  {
+  bool info_is_valid = true;
+  if (nullptr == info || nullptr == info->request_writer_ || nullptr == info->response_reader_) {
+    RMW_SET_ERROR_MSG("destroy_client() called with invalid info struct");
+    final_ret = RMW_RET_INVALID_ARGUMENT;
+    info_is_valid = false;
+  } else if (nullptr == participant_info || nullptr == common_context) {
+    RMW_SET_ERROR_MSG("destroy_client() called on invalid context");
+    final_ret = RMW_RET_INVALID_ARGUMENT;
+    info_is_valid = false;
+  }
+
+  if (info_is_valid) {
     // Update graph
     std::lock_guard<std::mutex> guard(common_context->node_update_mutex);
     rmw_gid_t gid = rmw_fastrtps_shared_cpp::create_rmw_gid(
@@ -63,12 +76,17 @@ __rmw_destroy_client(
       nullptr);
   }
 
+  auto show_previous_error = [&final_ret]() {
+    if (RMW_RET_OK != final_ret) {
+      RMW_SAFE_FWRITE_TO_STDERR(rmw_get_error_string().str);
+      RMW_SAFE_FWRITE_TO_STDERR(" during '" RCUTILS_STRINGIFY(__function__) "'\n");
+      rmw_reset_error();
+    }
+  };
+
   /////
   // Delete DataWriter and DataReader
-  auto participant_info =
-    static_cast<CustomParticipantInfo *>(node->context->impl->participant_info);
-
-  if (nullptr != info) {
+  if (info_is_valid) {
     std::lock_guard<std::mutex> lck(participant_info->entity_creation_mutex_);
 
     // Keep pointers to topics, so we can remove them later
@@ -78,8 +96,10 @@ __rmw_destroy_client(
     // Delete DataReader
     ReturnCode_t ret = participant_info->subscriber_->delete_datareader(info->response_reader_);
     if (ret != ReturnCode_t::RETCODE_OK) {
-      RMW_SET_ERROR_MSG("Fail in delete datareader");
-      return rmw_fastrtps_shared_cpp::cast_error_dds_to_rmw(ret);
+      show_previous_error();
+      RMW_SET_ERROR_MSG("destroy_client() failed to delete datareader");
+      final_ret = rmw_fastrtps_shared_cpp::cast_error_dds_to_rmw(ret);
+      info->response_reader_->set_listener(nullptr);
     }
 
     // Delete DataReader listener
@@ -90,8 +110,10 @@ __rmw_destroy_client(
     // Delete DataWriter
     ret = participant_info->publisher_->delete_datawriter(info->request_writer_);
     if (ret != ReturnCode_t::RETCODE_OK) {
-      RMW_SET_ERROR_MSG("Fail in delete datawriter");
-      return rmw_fastrtps_shared_cpp::cast_error_dds_to_rmw(ret);
+      show_previous_error();
+      RMW_SET_ERROR_MSG("destroy_client() failed to delete datawriter");
+      final_ret = rmw_fastrtps_shared_cpp::cast_error_dds_to_rmw(ret);
+      info->request_writer_->set_listener(nullptr);
     }
 
     // Delete DataWriter listener
@@ -105,8 +127,6 @@ __rmw_destroy_client(
 
     // Delete CustomClientInfo structure
     delete info;
-  } else {
-    final_ret = RMW_RET_INVALID_ARGUMENT;
   }
 
   rmw_free(const_cast<char *>(client->service_name));
