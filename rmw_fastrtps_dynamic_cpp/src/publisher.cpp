9b5785ec41a292adb5b4543e642afa0bf1d45236
Miguel Company <MiguelCompany@eprosima.com>
2021-11-05 09:24:31 +0100

    Allow publishers to loan messages without datasharing
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- rmw_fastrtps_dynamic_cpp/src/publisher.cpp
+++ rmw_fastrtps_dynamic_cpp/src/publisher.cpp
@@ -309,8 +309,7 @@ rmw_fastrtps_dynamic_cpp::create_publisher(
       rmw_publisher_free(rmw_publisher);
     });
 
-  bool has_data_sharing = DataSharingKind::OFF != writer_qos.data_sharing().kind();
-  rmw_publisher->can_loan_messages = has_data_sharing && info->type_support_->is_plain();
+  rmw_publisher->can_loan_messages = info->type_support_->is_plain();
   rmw_publisher->implementation_identifier = eprosima_fastrtps_identifier;
   rmw_publisher->data = info;
 
