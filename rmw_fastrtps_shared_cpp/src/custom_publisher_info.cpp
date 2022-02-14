e4b2cdd7fe5bca579897c60b320707ef93ef307b
Miguel Company <MiguelCompany@eprosima.com>
2022-02-14 09:01:03 +0100

    Add RMW_EVENT_OFFERED_QOS_INCOMPATIBLE to hasEvent and takeNextEvent.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- rmw_fastrtps_shared_cpp/src/custom_publisher_info.cpp
+++ rmw_fastrtps_shared_cpp/src/custom_publisher_info.cpp
@@ -13,8 +13,11 @@
 // limitations under the License.
 
 #include "rmw_fastrtps_shared_cpp/custom_publisher_info.hpp"
+
 #include "fastdds/dds/core/status/BaseStatus.hpp"
 #include "fastdds/dds/core/status/DeadlineMissedStatus.hpp"
+
+#include "event_helpers.hpp"
 #include "types/event_types.hpp"
 
 EventListenerInterface *
@@ -87,6 +90,8 @@ bool PubListener::hasEvent(rmw_event_type_t event_type) const
       return liveliness_changes_.load(std::memory_order_relaxed);
     case RMW_EVENT_OFFERED_DEADLINE_MISSED:
       return deadline_changes_.load(std::memory_order_relaxed);
+    case RMW_EVENT_OFFERED_QOS_INCOMPATIBLE:
+      return incompatible_qos_changes_.load(std::memory_order_relaxed);
     default:
       break;
   }
@@ -114,6 +119,18 @@ bool PubListener::takeNextEvent(rmw_event_type_t event_type, void * event_info)
         rmw_data->total_count_change = offered_deadline_missed_status_.total_count_change;
         offered_deadline_missed_status_.total_count_change = 0;
         deadline_changes_.store(false, std::memory_order_relaxed);
+      }
+      break;
+    case RMW_EVENT_OFFERED_QOS_INCOMPATIBLE:
+      {
+        auto rmw_data = static_cast<rmw_requested_qos_incompatible_event_status_t *>(event_info);
+        rmw_data->total_count = incompatible_qos_status_.total_count;
+        rmw_data->total_count_change = incompatible_qos_status_.total_count_change;
+        rmw_data->last_policy_kind =
+          rmw_fastrtps_shared_cpp::internal::dds_qos_policy_to_rmw_qos_policy(
+          incompatible_qos_status_.last_policy_id);
+        incompatible_qos_status_.total_count_change = 0;
+        incompatible_qos_changes_.store(false, std::memory_order_relaxed);
       }
       break;
     default:
