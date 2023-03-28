f055b23dd7fe7f6c4108b3d8fb723c890ca62511
Shane Loretz <sloretz@google.com>
2023-03-28 22:59:07 +0000

    Minimize diff with rolling in participant.cpp
    Signed-off-by: Shane Loretz <sloretz@google.com>

--- rmw_fastrtps_shared_cpp/src/participant.cpp
+++ rmw_fastrtps_shared_cpp/src/participant.cpp
@@ -59,62 +59,61 @@
 #include "rmw_dds_common/security.hpp"
 
 // Private function to create Participant with QoS
-static CustomParticipantInfo *__create_participant(
-    const char *identifier,
-    const eprosima::fastdds::dds::DomainParticipantQos &domainParticipantQos,
-    bool leave_middleware_default_qos, publishing_mode_t publishing_mode,
-    rmw_dds_common::Context *common_context, size_t domain_id,
-    const rmw_discovery_options_t *discovery_options, const char *hostname) {
-  CustomParticipantInfo *participant_info = nullptr;
+static CustomParticipantInfo *
+__create_participant(
+  const char * identifier,
+  const eprosima::fastdds::dds::DomainParticipantQos & domainParticipantQos,
+  bool leave_middleware_default_qos,
+  publishing_mode_t publishing_mode,
+  rmw_dds_common::Context * common_context,
+  size_t domain_id,
+  const rmw_discovery_options_t *discovery_options,
+  const char *hostname)
+{
+  CustomParticipantInfo * participant_info = nullptr;
 
   /////
   // Create Custom Participant
   try {
     participant_info = new CustomParticipantInfo();
   } catch (std::bad_alloc &) {
-    RMW_SET_ERROR_MSG(
-        "__create_participant failed to allocate CustomParticipantInfo struct");
+    RMW_SET_ERROR_MSG("__create_participant failed to allocate CustomParticipantInfo struct");
     return nullptr;
   }
   // lambda to delete participant info
-  auto cleanup_participant_info =
-      rcpputils::make_scope_exit([participant_info]() {
-        if (nullptr != participant_info->participant_) {
-          participant_info->participant_->delete_publisher(
-              participant_info->publisher_);
-          eprosima::fastdds::dds::DomainParticipantFactory::get_instance()
-              ->delete_participant(participant_info->participant_);
-        }
-        delete participant_info->listener_;
-        delete participant_info;
-      });
+  auto cleanup_participant_info = rcpputils::make_scope_exit(
+    [participant_info]() {
+      if (nullptr != participant_info->participant_) {
+        participant_info->participant_->delete_publisher(participant_info->publisher_);
+        eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->delete_participant(
+          participant_info->participant_);
+      }
+      delete participant_info->listener_;
+      delete participant_info;
+    });
 
   /////
   // Create Participant listener
   try {
     participant_info->listener_ = new ParticipantListener(
-        identifier, common_context, hostname, discovery_options);
+      identifier, common_context, hostname, discovery_options);
   } catch (std::bad_alloc &) {
-    RMW_SET_ERROR_MSG(
-        "__create_participant failed to allocate participant listener");
+    RMW_SET_ERROR_MSG("__create_participant failed to allocate participant listener");
     return nullptr;
   }
 
   /////
   // Create Participant
 
-  // As the participant listener is only used for discovery related callbacks,
-  // which are Fast DDS extensions to the DDS standard DomainParticipantListener
-  // interface, an empty mask should be used to let child entities handle
-  // standard DDS events.
-  eprosima::fastdds::dds::StatusMask participant_mask =
-      eprosima::fastdds::dds::StatusMask::none();
+  // As the participant listener is only used for discovery related callbacks, which are
+  // Fast DDS extensions to the DDS standard DomainParticipantListener interface, an empty
+  // mask should be used to let child entities handle standard DDS events.
+  eprosima::fastdds::dds::StatusMask participant_mask = eprosima::fastdds::dds::StatusMask::none();
 
   participant_info->participant_ =
-      eprosima::fastdds::dds::DomainParticipantFactory::get_instance()
-          ->create_participant(static_cast<uint32_t>(domain_id),
-                               domainParticipantQos,
-                               participant_info->listener_, participant_mask);
+    eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->create_participant(
+    static_cast<uint32_t>(domain_id), domainParticipantQos,
+    participant_info->listener_, participant_mask);
 
   if (!participant_info->participant_) {
     RMW_SET_ERROR_MSG("__create_participant failed to create participant");
@@ -129,11 +128,10 @@ static CustomParticipantInfo *__create_participant(
   /////
   // Create Publisher
   eprosima::fastdds::dds::PublisherQos publisherQos =
-      participant_info->participant_->get_default_publisher_qos();
+    participant_info->participant_->get_default_publisher_qos();
   publisherQos.entity_factory(domainParticipantQos.entity_factory());
 
-  participant_info->publisher_ =
-      participant_info->participant_->create_publisher(publisherQos);
+  participant_info->publisher_ = participant_info->participant_->create_publisher(publisherQos);
   if (!participant_info->publisher_) {
     RMW_SET_ERROR_MSG("__create_participant could not create publisher");
     return nullptr;
@@ -142,11 +140,10 @@ static CustomParticipantInfo *__create_participant(
   /////
   // Create Subscriber
   eprosima::fastdds::dds::SubscriberQos subscriberQos =
-      participant_info->participant_->get_default_subscriber_qos();
+    participant_info->participant_->get_default_subscriber_qos();
   subscriberQos.entity_factory(domainParticipantQos.entity_factory());
 
-  participant_info->subscriber_ =
-      participant_info->participant_->create_subscriber(subscriberQos);
+  participant_info->subscriber_ = participant_info->participant_->create_subscriber(subscriberQos);
   if (!participant_info->subscriber_) {
     RMW_SET_ERROR_MSG("__create_participant could not create subscriber");
     return nullptr;
@@ -187,11 +184,15 @@ int ip_version(const std::string& peer) {
   return 4;
 }
 
-CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
-    const char *identifier, size_t domain_id,
-    const rmw_security_options_t *security_options,
-    const rmw_discovery_options_t *discovery_options, const char *enclave,
-    rmw_dds_common::Context *common_context) {
+CustomParticipantInfo *
+rmw_fastrtps_shared_cpp::create_participant(
+  const char * identifier,
+  size_t domain_id,
+  const rmw_security_options_t * security_options,
+  const rmw_discovery_options_t * discovery_options,
+  const char * enclave,
+  rmw_dds_common::Context * common_context)
+{
   RCUTILS_CAN_RETURN_WITH_ERROR_OF(nullptr);
 
   if (!security_options) {
@@ -200,11 +201,9 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
   }
 
   // Load default XML profile.
-  eprosima::fastdds::dds::DomainParticipantFactory::get_instance()
-      ->load_profiles();
+  eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->load_profiles();
   eprosima::fastdds::dds::DomainParticipantQos domainParticipantQos =
-      eprosima::fastdds::dds::DomainParticipantFactory::get_instance()
-          ->get_default_participant_qos();
+    eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->get_default_participant_qos();
 
   // Configure discovery
   switch (discovery_options->automatic_discovery_range) {
@@ -225,8 +224,7 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
   // Fall through to localhost behaviour
   case RMW_AUTOMATIC_DISCOVERY_RANGE_LOCALHOST: {
     // Create a descriptor for the new transport.
-    auto udp_transport =
-        std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
+    auto udp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
     udp_transport->TTL = 0;
     domainParticipantQos.transport().clear();
     domainParticipantQos.transport().user_transports.push_back(udp_transport);
@@ -285,8 +283,8 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
   auto length = user_data.str().length() + 1;
   domainParticipantQos.user_data().resize(length);
   int written = snprintf(
-      reinterpret_cast<char *>(domainParticipantQos.user_data().data()), length,
-      "%s", user_data.str().c_str());
+    reinterpret_cast<char *>(domainParticipantQos.user_data().data()),
+    length, "%s", user_data.str().c_str());
   if (written < 0 || written > static_cast<int>(length) - 1) {
     RMW_SET_ERROR_MSG("failed to populate user_data buffer");
     return nullptr;
@@ -296,12 +294,11 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
 
   bool leave_middleware_default_qos = false;
   publishing_mode_t publishing_mode = publishing_mode_t::SYNCHRONOUS;
-  const char *env_value;
-  const char *error_str;
+  const char * env_value;
+  const char * error_str;
   error_str = rcutils_get_env("RMW_FASTRTPS_USE_QOS_FROM_XML", &env_value);
   if (error_str != NULL) {
-    RCUTILS_LOG_DEBUG_NAMED("rmw_fastrtps_shared_cpp",
-                            "Error getting env var: %s\n", error_str);
+    RCUTILS_LOG_DEBUG_NAMED("rmw_fastrtps_shared_cpp", "Error getting env var: %s\n", error_str);
     return nullptr;
   }
   if (env_value != nullptr) {
@@ -310,8 +307,7 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
   if (!leave_middleware_default_qos) {
     error_str = rcutils_get_env("RMW_FASTRTPS_PUBLICATION_MODE", &env_value);
     if (error_str != NULL) {
-      RMW_SET_ERROR_MSG_WITH_FORMAT_STRING("Error getting env var: %s\n",
-                                           error_str);
+      RMW_SET_ERROR_MSG_WITH_FORMAT_STRING("Error getting env var: %s\n", error_str);
       return nullptr;
     }
     if (env_value != nullptr) {
@@ -322,59 +318,54 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
       } else if (strcmp(env_value, "AUTO") == 0) {
         publishing_mode = publishing_mode_t::AUTO;
       } else if (strcmp(env_value, "") != 0) {
-        RCUTILS_LOG_WARN_NAMED("rmw_fastrtps_shared_cpp",
-                               "Value %s unknown for environment variable "
-                               "RMW_FASTRTPS_PUBLICATION_MODE"
-                               ". Using default SYNCHRONOUS publishing mode.",
-                               env_value);
+        RCUTILS_LOG_WARN_NAMED(
+          "rmw_fastrtps_shared_cpp",
+          "Value %s unknown for environment variable RMW_FASTRTPS_PUBLICATION_MODE"
+          ". Using default SYNCHRONOUS publishing mode.", env_value);
       }
     }
   }
   // allow reallocation to support discovery messages bigger than 5000 bytes
   if (!leave_middleware_default_qos) {
     domainParticipantQos.wire_protocol().builtin.readerHistoryMemoryPolicy =
-        eprosima::fastrtps::rtps::PREALLOCATED_WITH_REALLOC_MEMORY_MODE;
+      eprosima::fastrtps::rtps::PREALLOCATED_WITH_REALLOC_MEMORY_MODE;
     domainParticipantQos.wire_protocol().builtin.writerHistoryMemoryPolicy =
-        eprosima::fastrtps::rtps::PREALLOCATED_WITH_REALLOC_MEMORY_MODE;
+      eprosima::fastrtps::rtps::PREALLOCATED_WITH_REALLOC_MEMORY_MODE;
   }
   if (security_options->security_root_path) {
     // if security_root_path provided, try to find the key and certificate files
 #if HAVE_SECURITY
     std::unordered_map<std::string, std::string> security_files_paths;
-    if (rmw_dds_common::get_security_files("file://",
-                                           security_options->security_root_path,
-                                           security_files_paths)) {
+    if (rmw_dds_common::get_security_files(
+        "file://", security_options->security_root_path, security_files_paths))
+    {
       eprosima::fastrtps::rtps::PropertyPolicy property_policy;
-      property_policy.properties().emplace_back("dds.sec.auth.plugin",
-                                                "builtin.PKI-DH");
       property_policy.properties().emplace_back(
-          "dds.sec.auth.builtin.PKI-DH.identity_ca",
-          security_files_paths["IDENTITY_CA"]);
+        "dds.sec.auth.plugin", "builtin.PKI-DH");
       property_policy.properties().emplace_back(
-          "dds.sec.auth.builtin.PKI-DH.identity_certificate",
-          security_files_paths["CERTIFICATE"]);
+        "dds.sec.auth.builtin.PKI-DH.identity_ca", security_files_paths["IDENTITY_CA"]);
       property_policy.properties().emplace_back(
-          "dds.sec.auth.builtin.PKI-DH.private_key",
-          security_files_paths["PRIVATE_KEY"]);
-      property_policy.properties().emplace_back("dds.sec.crypto.plugin",
-                                                "builtin.AES-GCM-GMAC");
+        "dds.sec.auth.builtin.PKI-DH.identity_certificate", security_files_paths["CERTIFICATE"]);
+      property_policy.properties().emplace_back(
+        "dds.sec.auth.builtin.PKI-DH.private_key", security_files_paths["PRIVATE_KEY"]);
+      property_policy.properties().emplace_back(
+        "dds.sec.crypto.plugin", "builtin.AES-GCM-GMAC");
 
-      property_policy.properties().emplace_back("dds.sec.access.plugin",
-                                                "builtin.Access-Permissions");
       property_policy.properties().emplace_back(
-          "dds.sec.access.builtin.Access-Permissions.permissions_ca",
-          security_files_paths["PERMISSIONS_CA"]);
+        "dds.sec.access.plugin", "builtin.Access-Permissions");
       property_policy.properties().emplace_back(
-          "dds.sec.access.builtin.Access-Permissions.governance",
-          security_files_paths["GOVERNANCE"]);
+        "dds.sec.access.builtin.Access-Permissions.permissions_ca",
+        security_files_paths["PERMISSIONS_CA"]);
       property_policy.properties().emplace_back(
-          "dds.sec.access.builtin.Access-Permissions.permissions",
-          security_files_paths["PERMISSIONS"]);
+        "dds.sec.access.builtin.Access-Permissions.governance",
+        security_files_paths["GOVERNANCE"]);
+      property_policy.properties().emplace_back(
+        "dds.sec.access.builtin.Access-Permissions.permissions",
+        security_files_paths["PERMISSIONS"]);
 
       if (security_files_paths.count("CRL") > 0) {
         property_policy.properties().emplace_back(
-            "dds.sec.auth.builtin.PKI-DH.identity_crl",
-            security_files_paths["CRL"]);
+          "dds.sec.auth.builtin.PKI-DH.identity_crl", security_files_paths["CRL"]);
       }
 
       // Configure security logging
@@ -389,18 +380,25 @@ CustomParticipantInfo *rmw_fastrtps_shared_cpp::create_participant(
     }
 #else
     RMW_SET_ERROR_MSG(
-        "This Fast DDS version doesn't have the security libraries\n"
-        "Please compile Fast DDS using the -DSECURITY=ON CMake option");
+      "This Fast DDS version doesn't have the security libraries\n"
+      "Please compile Fast DDS using the -DSECURITY=ON CMake option");
     return nullptr;
 #endif
   }
   return __create_participant(
-      identifier, domainParticipantQos, leave_middleware_default_qos,
-      publishing_mode, common_context, domain_id, discovery_options, hostname);
+    identifier,
+    domainParticipantQos,
+    leave_middleware_default_qos,
+    publishing_mode,
+    common_context,
+    domain_id,
+    discovery_options,
+    hostname);
 }
 
-rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
-    CustomParticipantInfo *participant_info) {
+rmw_ret_t
+rmw_fastrtps_shared_cpp::destroy_participant(CustomParticipantInfo * participant_info)
+{
   if (!participant_info) {
     RMW_SET_ERROR_MSG("participant_info is null on destroy_participant");
     return RMW_RET_ERROR;
@@ -412,8 +410,7 @@ rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
   ReturnCode_t ret = ReturnCode_t::RETCODE_OK;
 
   // Collect topics that should be deleted
-  std::vector<const eprosima::fastdds::dds::TopicDescription *>
-      topics_to_remove;
+  std::vector<const eprosima::fastdds::dds::TopicDescription *> topics_to_remove;
 
   // Remove datawriters and publisher from participant
   {
@@ -423,11 +420,9 @@ rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
       topics_to_remove.push_back(writer->get_topic());
       participant_info->publisher_->delete_datawriter(writer);
     }
-    ret = participant_info->participant_->delete_publisher(
-        participant_info->publisher_);
+    ret = participant_info->participant_->delete_publisher(participant_info->publisher_);
     if (ReturnCode_t::RETCODE_OK != ret) {
-      RCUTILS_SAFE_FWRITE_TO_STDERR(
-          "Failed to delete dds publisher from participant");
+      RCUTILS_SAFE_FWRITE_TO_STDERR("Failed to delete dds publisher from participant");
     }
   }
 
@@ -439,11 +434,9 @@ rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
       topics_to_remove.push_back(reader->get_topicdescription());
       participant_info->subscriber_->delete_datareader(reader);
     }
-    ret = participant_info->participant_->delete_subscriber(
-        participant_info->subscriber_);
+    ret = participant_info->participant_->delete_subscriber(participant_info->subscriber_);
     if (ReturnCode_t::RETCODE_OK != ret) {
-      RCUTILS_SAFE_FWRITE_TO_STDERR(
-          "Failed to delete dds subscriber from participant");
+      RCUTILS_SAFE_FWRITE_TO_STDERR("Failed to delete dds subscriber from participant");
     }
   }
 
@@ -459,8 +452,9 @@ rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
   }
 
   // Delete Domain Participant
-  ret = eprosima::fastdds::dds::DomainParticipantFactory::get_instance()
-            ->delete_participant(participant_info->participant_);
+  ret =
+    eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->delete_participant(
+    participant_info->participant_);
 
   if (ReturnCode_t::RETCODE_OK != ret) {
     RCUTILS_SAFE_FWRITE_TO_STDERR("Failed to delete participant");
@@ -472,7 +466,7 @@ rmw_ret_t rmw_fastrtps_shared_cpp::destroy_participant(
   // Delete Custom Participant
   delete participant_info;
 
-  RCUTILS_CAN_RETURN_WITH_ERROR_OF(RMW_RET_ERROR); // on completion
+  RCUTILS_CAN_RETURN_WITH_ERROR_OF(RMW_RET_ERROR);  // on completion
 
   return RMW_RET_OK;
 }
