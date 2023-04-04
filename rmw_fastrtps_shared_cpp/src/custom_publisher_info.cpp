5a2fc030291935b13bb28bed324f5dc70e8b3b70
Miguel Company <MiguelCompany@eprosima.com>
2023-04-04 16:11:49 +0200

    Make update_matched only do its processing when event callback has been set.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- rmw_fastrtps_shared_cpp/src/custom_publisher_info.cpp
+++ rmw_fastrtps_shared_cpp/src/custom_publisher_info.cpp
@@ -360,14 +360,16 @@ void RMWPublisherEvent::update_matched(
 {
   std::lock_guard<std::mutex> lock(on_new_event_m_);
 
-  matched_status_.total_count = total_count;
-  matched_status_.total_count_change += total_count_change;
-  matched_status_.current_count = current_count;
-  matched_status_.current_count_change += current_count_change;
+  if (on_new_event_cb_[RMW_EVENT_PUBLICATION_MATCHED]) {
+    matched_status_.total_count = total_count;
+    matched_status_.total_count_change += total_count_change;
+    matched_status_.current_count = current_count;
+    matched_status_.current_count_change += current_count_change;
 
-  matched_changes_ = true;
+    matched_changes_ = true;
 
-  trigger_event(RMW_EVENT_PUBLICATION_MATCHED);
+    trigger_event(RMW_EVENT_PUBLICATION_MATCHED);
+  }
 }
 
 void RMWPublisherEvent::trigger_event(rmw_event_type_t event_type)
