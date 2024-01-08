202244ec84f920741242861283ff9e3f28cc1893
Miguel Company <miguelcompany@eprosima.com>
2024-01-08 21:08:07 +0100

    Capture `std::bad_alloc` on deserializeROSmessage. (#665)
    * Capture bad_alloc on deserializeROSmessage.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    (cherry picked from commit 4d0be32e6c455edbf708003dffb67b11d512c5a6)

--- rmw_fastrtps_dynamic_cpp/include/rmw_fastrtps_dynamic_cpp/TypeSupport_impl.hpp
+++ rmw_fastrtps_dynamic_cpp/include/rmw_fastrtps_dynamic_cpp/TypeSupport_impl.hpp
@@ -983,6 +983,11 @@ bool TypeSupport<MembersType>::deserializeROSmessage(
       "Fast CDR exception deserializing message of type %s.",
       getName());
     return false;
+  } catch (const std::bad_alloc &) {
+    RMW_SET_ERROR_MSG_WITH_FORMAT_STRING(
+      "'Bad alloc' exception deserializing message of type %s.",
+      getName());
+    return false;
   }
 
   return true;
