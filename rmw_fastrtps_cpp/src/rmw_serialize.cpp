d0dddc1669efddd0af7b461637d404f05ea9fca8
Miguel Company <miguelcompany@eprosima.com>
2024-02-12 17:03:16 +0100

    Changes to build rmw_fastrtps_cpp
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- rmw_fastrtps_cpp/src/rmw_serialize.cpp
+++ rmw_fastrtps_cpp/src/rmw_serialize.cpp
@@ -52,7 +52,8 @@ rmw_serialize(
   eprosima::fastcdr::FastBuffer buffer(
     reinterpret_cast<char *>(serialized_message->buffer), data_length);
   eprosima::fastcdr::Cdr ser(
-    buffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN, eprosima::fastcdr::Cdr::DDS_CDR);
+    buffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN, eprosima::fastcdr::CdrVersion::XCDRv1);
+  ser.set_encoding_flag(eprosima::fastcdr::EncodingAlgorithmFlag::PLAIN_CDR);
 
   auto ret = tss.serializeROSmessage(ros_message, ser, callbacks);
   serialized_message->buffer_length = data_length;
@@ -81,8 +82,7 @@ rmw_deserialize(
   auto tss = MessageTypeSupport_cpp(callbacks);
   eprosima::fastcdr::FastBuffer buffer(
     reinterpret_cast<char *>(serialized_message->buffer), serialized_message->buffer_length);
-  eprosima::fastcdr::Cdr deser(buffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN,
-    eprosima::fastcdr::Cdr::DDS_CDR);
+  eprosima::fastcdr::Cdr deser(buffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN);
 
   auto ret = tss.deserializeROSmessage(deser, ros_message, callbacks);
   return ret == true ? RMW_RET_OK : RMW_RET_ERROR;
