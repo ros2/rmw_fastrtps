ffb4386254e976fa940936024fb4f0e025da54a4
Lucas Wendland <82680922+CursedRock17@users.noreply.github.com>
2024-05-02 21:17:46 -0400

    Typo causing error fix
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>
    Signed-off-by: Lucas Wendland <82680922+CursedRock17@users.noreply.github.com>

--- rmw_fastrtps_shared_cpp/src/qos.cpp
+++ rmw_fastrtps_shared_cpp/src/qos.cpp
@@ -142,7 +142,7 @@ bool fill_entity_qos_from_profile(
     // See doc at https://github.com/eProsima/Fast-RTPS/blob/
     //   a8691a40be6b8460b01edde36ad8563170a3a35a/include/fastrtps/qos/QosPolicies.h#L223-L232
     int64_t period_in_ns = entity_qos.liveliness().lease_duration.to_ns() * 2 / 3;
-    double period_in_s = RCUITLS_NS_TO_S(period_in_s);
+    double period_in_s = RCUTILS_NS_TO_S(period_in_ns);
     entity_qos.liveliness().announcement_period = eprosima::fastrtps::Duration_t(period_in_s);
   }
 
