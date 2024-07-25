d6afb7ad4ef9e3e8743703046201af3e507e31ce
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-07-25 16:43:28 -0700

    remove rmw_localhost_only_t.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rmw_fastrtps_shared_cpp/src/rmw_init.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_init.cpp
@@ -43,7 +43,6 @@ rmw_init_options_init(
   init_options->enclave = NULL;
   init_options->domain_id = RMW_DEFAULT_DOMAIN_ID;
   init_options->security_options = rmw_get_default_security_options();
-  init_options->localhost_only = RMW_LOCALHOST_ONLY_DEFAULT;
   init_options->discovery_options = rmw_get_zero_initialized_discovery_options();
   return rmw_discovery_options_init(&(init_options->discovery_options), 0, &allocator);
 }
