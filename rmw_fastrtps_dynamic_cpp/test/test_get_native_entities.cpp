7be33a46ef075dbd40112fd257aa110a6308808f
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-03-03 10:41:57 -0800

    fix rmw_fastrtps_dynamic_cpp test build failure.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rmw_fastrtps_dynamic_cpp/test/test_get_native_entities.cpp
+++ rmw_fastrtps_dynamic_cpp/test/test_get_native_entities.cpp
@@ -36,15 +36,16 @@ class TestNativeEntities : public ::testing::Test
 protected:
   void SetUp() override
   {
+    rcutils_allocator_t allocator = rcutils_get_default_allocator();
     rmw_init_options_t options = rmw_get_zero_initialized_init_options();
-    rmw_ret_t ret = rmw_init_options_init(&options, rcutils_get_default_allocator());
+    rmw_ret_t ret = rmw_init_options_init(&options, allocator);
     ASSERT_EQ(RMW_RET_OK, ret) << rcutils_get_error_string().str;
     OSRF_TESTING_TOOLS_CPP_SCOPE_EXIT(
     {
       rmw_ret_t ret = rmw_init_options_fini(&options);
       EXPECT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
     });
-    ret = rmw_enclave_options_copy("/", &rcutils_get_default_allocator(), &options.enclave);
+    ret = rmw_enclave_options_copy("/", &allocator, &options.enclave);
     ASSERT_EQ(RMW_RET_OK, ret) << rmw_get_error_string().str;
     ASSERT_STREQ("/", options.enclave);
     options.discovery_options.automatic_discovery_range = RMW_AUTOMATIC_DISCOVERY_RANGE_OFF;
