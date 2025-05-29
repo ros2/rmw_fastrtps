d688eb1d3035eefe5bbd6518d1cc6fa0857a9f70
Mario Dominguez <mariodominguez@eprosima.com>
2025-05-29 07:10:42 +0000

    Retrieve HistoryQoS in discovery when available
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/qos.hpp
+++ rmw_fastrtps_shared_cpp/include/rmw_fastrtps_shared_cpp/qos.hpp
@@ -175,6 +175,27 @@ rtps_qos_to_rmw_qos(
       break;
   }
   qos->liveliness_lease_duration = dds_duration_to_rmw(rtps_qos.liveliness.lease_duration);
+
+  if (rtps_qos.history.has_value())
+  {
+    if (rtps_qos.history->kind == eprosima::fastdds::dds::KEEP_LAST_HISTORY_QOS)
+    {
+      qos->history = RMW_QOS_POLICY_HISTORY_KEEP_LAST;
+    }
+    else if (rtps_qos.history->kind == eprosima::fastdds::dds::KEEP_ALL_HISTORY_QOS)
+    {
+      qos->history = RMW_QOS_POLICY_HISTORY_KEEP_ALL;
+    }
+    else
+    {
+      qos->history = RMW_QOS_POLICY_HISTORY_UNKNOWN;
+    }
+    qos->depth = static_cast<size_t>(rtps_qos.history->depth);
+  }
+  else
+  {
+    qos->history = RMW_QOS_POLICY_HISTORY_UNKNOWN;
+  }
 }
 
 extern template RMW_FASTRTPS_SHARED_CPP_PUBLIC
