c487a00a52f180dcf2f32c260fec4dacb329170e
Miguel Company <miguelcompany@eprosima.com>
2026-09-08 17:44:42 +0200

    Fixes in `rmw_wait` (#895)
    * fix: Fix race condition when untriggering guard conditions.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * fix: return `RMW_RET_OK` whenever an active element is returned
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * style: uncrustify
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    (cherry picked from commit 83471d45c448dfc7f4d408bc36cff593df14eb90)

--- rmw_fastrtps_shared_cpp/src/rmw_wait.cpp
+++ rmw_fastrtps_shared_cpp/src/rmw_wait.cpp
@@ -141,6 +141,10 @@ __rmw_wait(
         custom_subscriber_info->data_reader_->get_first_untaken_info(&sample_info))
       {
         subscriptions->subscribers[i] = 0;
+      } else {
+        // We are returning a ready subscription,
+        // so we need to indicate that the wait was successful.
+        wait_result = true;
       }
     }
   }
@@ -157,6 +161,10 @@ __rmw_wait(
         custom_client_info->response_reader_->get_first_untaken_info(&sample_info))
       {
         clients->clients[i] = 0;
+      } else {
+        // We are returning a ready client,
+        // so we need to indicate that the wait was successful.
+        wait_result = true;
       }
     }
   }
@@ -173,6 +181,10 @@ __rmw_wait(
         custom_service_info->request_reader_->get_first_untaken_info(&sample_info))
       {
         services->services[i] = 0;
+      } else {
+        // We are returning a ready service,
+        // so we need to indicate that the wait was successful.
+        wait_result = true;
       }
     }
   }
@@ -208,6 +220,10 @@ __rmw_wait(
 
       if (!active) {
         events->events[i] = 0;
+      } else {
+        // We are returning a ready event,
+        // so we need to indicate that the wait was successful.
+        wait_result = true;
       }
     }
   }
@@ -219,8 +235,12 @@ __rmw_wait(
       fastdds_wait_set->detach_condition(*condition);
       if (!condition->get_trigger_value()) {
         guard_conditions->guard_conditions[i] = 0;
+      } else {
+        condition->set_trigger_value(false);
+        // We are returning a ready guard condition,
+        // so we need to indicate that the wait was successful.
+        wait_result = true;
       }
-      condition->set_trigger_value(false);
     }
   }
 
